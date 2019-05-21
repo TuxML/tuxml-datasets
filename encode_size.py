4094f3f0469614c1c7a80fb1c7635e80a145907f
Hugo Martin <hugojp.martin@yahoo.fr>
2019-05-21 11:20:53 +0000

    Added encoded datasets for size

---
+++ encode_size.py
@@ -0,0 +1,80 @@
+import pandas as pd
+
+list_csv=[
+    "config_bdd15000-20000.csv",
+    "config_bdd20000-30000.csv",
+    "config_bdd30000-40000.csv",
+    "config_bdd40000-60000.csv",
+    "config_bdd60000-90000.csv",
+    "config_bdd90000-100000.csv",
+    "config_bdd100000-1265000.csv",
+]
+
+for csv in list_csv:
+
+    df = pd.read_csv(csv)
+
+    basic_head = ["cid", "time", "date"] # "compile"
+    size_methods = ["vmlinux", "GZIP-bzImage", "GZIP-vmlinux", "GZIP", "BZIP2-bzImage", 
+                  "BZIP2-vmlinux", "BZIP2", "LZMA-bzImage", "LZMA-vmlinux", "LZMA", "XZ-bzImage", "XZ-vmlinux", "XZ", 
+                  "LZO-bzImage", "LZO-vmlinux", "LZO", "LZ4-bzImage", "LZ4-vmlinux", "LZ4"]
+    compilation_status_column_name = 'kernel_size'
+
+    #Filtering out X86_32 configuration
+    df.query("X86_64 == 'y'", inplace=True)
+
+    #Filtering out specific cid
+    df.query("cid != 74459", inplace=True)
+    df.query("cid != 30698", inplace=True)
+
+    #tri state step
+    tri_state_values = ['y', 'n', 'm']
+
+    ftuniques = []
+    freq_ymn_features = []
+    non_tristate_options = []
+
+    for col in df:
+        ft = df[col]    
+        # eg always "y"
+        if len(ft.unique()) == 1:
+            ftuniques.append(col)
+        # only tri-state values (y, n, m) (possible TODO: handle numerical/string options)    
+        elif all(x in tri_state_values for x in ft.unique()):     #len(ft.unique()) == 3: 
+            freq = ft.value_counts(normalize=True)
+            freqy = 0
+            freqn = 0
+            freqm = 0
+            if ('y' in freq.index):
+                freqy = freq['y']
+            if ('n' in freq.index):
+                freqn = freq['n']
+            if ('m' in freq.index):
+                freqm = freq['m']
+            freq_ymn_features.append((col, freqy, freqm, freqn))
+        else:
+            if not (col in size_methods): 
+                non_tristate_options.append(col)
+
+    from sklearn.preprocessing import LabelEncoder
+    def encode_data_compilation(df):
+        lae = LabelEncoder()
+        # we save quantitative values we want (here vmlinux, TODO: generalize)
+        # the key idea is that the labelling encoder should not be applied to this kind of values (only to predictor variables!)
+        # vml = rawtuxdata['LZO'] # rawtuxdata['vmlinux'] 
+        o_sizes = df[size_methods]
+        cid = df["cid"]
+        # we may remove non tri state options, but TODO there are perhaps some interesting options (numerical or string) here
+        #tuxdata = rawtuxdata.drop(columns=non_tristate_options).drop(columns=['vmlinux']).apply(le.fit_transform)
+        df_encoded = df.drop(columns=non_tristate_options).drop(columns=size_methods).apply(lae.fit_transform)
+
+        #tuxdata['vmlinux'] = vml 
+        #df_encoded[size_methods] = o_sizes
+        # we can ue vmlinux since it has been restored thanks to previous line
+        df_encoded[compilation_status_column_name] = o_sizes['vmlinux']
+        df_encoded["cid"]=cid
+        return df_encoded
+
+    df_encoded = encode_data_compilation(df)
+
+    df_encoded.to_csv(csv.split(".")[0]+"_encoded_size.csv", index=False)
---
+++ fuse_size.py
@@ -0,0 +1,65 @@
+import pandas as pd
+
+df1 = pd.read_csv("config_bdd15000-20000_encoded_size.csv")
+df2 = pd.read_csv("config_bdd20000-30000_encoded_size.csv")
+df3 = pd.read_csv("config_bdd30000-40000_encoded_size.csv")
+df4 = pd.read_csv("config_bdd40000-60000_encoded_size.csv")
+df5 = pd.read_csv("config_bdd60000-90000_encoded_size.csv")
+df6 = pd.read_csv("config_bdd90000-100000_encoded_size.csv")
+df7 = pd.read_csv("config_bdd100000-1265000_encoded_size.csv")
+
+df = pd.concat([df1,df2,df3,df4,df5,df6,df7])
+
+#Save some memory
+del(df1)
+del(df2)
+del(df3)
+del(df4)
+del(df5)
+del(df6)
+del(df7)
+
+basic_head = ["cid", "time", "date"] # "compile"
+size_methods = ["vmlinux", "GZIP-bzImage", "GZIP-vmlinux", "GZIP", "BZIP2-bzImage", 
+              "BZIP2-vmlinux", "BZIP2", "LZMA-bzImage", "LZMA-vmlinux", "LZMA", "XZ-bzImage", "XZ-vmlinux", "XZ", 
+              "LZO-bzImage", "LZO-vmlinux", "LZO", "LZ4-bzImage", "LZ4-vmlinux", "LZ4"]
+compilation_status_column_name = 'kernel_size'
+
+#Find tri state options
+#tri state step
+tri_state_values = [0, 1, 2]
+
+ftuniques = []
+freq_ymn_features = []
+non_tristate_options = []
+
+for col in df:
+    ft = df[col]    
+    # eg always "y"
+    if len(ft.unique()) == 1:
+        ftuniques.append(col)
+    # only tri-state values (y, n, m) (possible TODO: handle numerical/string options)    
+    elif all(x in tri_state_values for x in ft.unique()):     #len(ft.unique()) == 3: 
+        freq = ft.value_counts(normalize=True)
+        freq0 = 0
+        freq1 = 0
+        freq2 = 0
+        if (0 in freq.index):
+            freq0 = freq[0]
+        if (1 in freq.index):
+            freqn = freq[1]
+        if (2 in freq.index):
+            freqm = freq[2]
+        freq_ymn_features.append((col, freq0, freq1, freq2))
+    else:
+        if not (col in size_methods): 
+            non_tristate_options.append(col)
+        
+
+
+print(str(len(df)) + " before the removal of some entries (those with same configurations)")
+#Dropping duplicates
+df.drop_duplicates(subset=df.columns.difference(ftuniques).difference(size_methods).difference(basic_head).difference(non_tristate_options), inplace=True)
+print(str(len(df)) + " after the removal of some entries (those with same configurations)")
+
+df.to_csv("dataset_encoded_size.csv", index=False)
